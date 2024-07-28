abffee69f3d1386bab71f7b20be41ee0d46355d0
venuazure <148867176+venuazure@users.noreply.github.com>
2024-07-28 21:06:12 +0530

    Create variables.tf

---
+++ simple/variables.tf
@@ -0,0 +1,12 @@
+variable "google_credentials" {
+  description = "Base64 encoded Google Cloud service account key"
+}
+
+variable "google_project" {
+  description = "Google Cloud project ID"
+}
+
+variable "google_region" {
+  description = "Google Cloud region"
+  default     = "us-central1"  # or your preferred default region
+}
