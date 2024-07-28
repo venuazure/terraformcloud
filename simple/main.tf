b49f41a050ceb68901666c7a8ffa0bed4b67ab2e
vijay <velagav4@outlook.com>
2024-07-28 15:03:20 +0530

    added code

--- simple/main.tf
+++ simple/main.tf
@@ -1,39 +1,9 @@
-/*
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "5.38.0"
-    }
-  }
-}*/
+data "google_credentials" "default" {
+  json_key_base64 = var.google_credentials
+}
 
 provider "google" {
-  credentials = var.google_credentials
-
-}
-resource "google_compute_network" "vpc_network" {
-
-  name                    = var.vpc_network_name
-  auto_create_subnetworks = true
+  credentials = data.google_credentials.default.json_key_base64
+  project     = data.google_credentials.default.project_id
 }
 
-variable "google_credentials" {
-  description = "Base64-encoded contents of the service account JSON key file"
-  type        = string
-}
-
-variable "project" {
-  description = "The ID of the GCP project to use"
-  type        = string
-}
-
-variable "region" {
-  description = "The region to create resources in"
-  type        = string
-}
-
-variable "vpc_network_name" {
-  description = "The name of the VPC network to create"
-  type        = string
-}
