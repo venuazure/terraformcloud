49e7780947089d2ff93d5acdf72cdedf41d19b5e
venuazure <148867176+venuazure@users.noreply.github.com>
2024-07-28 20:58:11 +0530

    Update main.tf

--- simple/main.tf
+++ simple/main.tf
@@ -1,3 +1,21 @@
+provider "google" {
+  credentials = base64decode(var.google_credentials)
+  project     = var.google_project
+  region      = var.google_region
+}
+
+variable "google_credentials" {
+  description = "Base64 encoded Google Cloud service account key"
+}
+
+variable "google_project" {
+  description = "Google Cloud project ID"
+}
+
+variable "google_region" {
+  description = "Google Cloud region"
+  default     = "us-central1"  # or your preferred default region
+}
 
 
 
