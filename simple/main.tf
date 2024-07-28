0369c03949ac715eb5a901cc97810fbd8687c931
vijay <velagav4@outlook.com>
2024-07-28 10:35:47 +0530

    added code

---
+++ simple/main.tf
@@ -0,0 +1,30 @@
+provider "google" {
+  credentials = base64decode(var.google_credentials)
+  project     = var.project
+  region      = var.region
+}
+
+resource "google_compute_network" "vpc_network" {
+  name                    = var.vpc_network_name
+  auto_create_subnetworks = true
+}
+
+variable "google_credentials" {
+  description = "Base64-encoded contents of the service account JSON key file"
+  type        = string
+}
+
+variable "project" {
+  description = "The ID of the GCP project to use"
+  type        = string
+}
+
+variable "region" {
+  description = "The region to create resources in"
+  type        = string
+}
+
+variable "vpc_network_name" {
+  description = "The name of the VPC network to create"
+  type        = string
+}
