9c7bac1ba10524ca053f8f9d0e9449ea5f969286
venuazure <148867176+venuazure@users.noreply.github.com>
2024-07-28 21:01:01 +0530

    Update main.tf

--- simple/main.tf
+++ simple/main.tf
@@ -4,17 +4,9 @@ provider "google" {
   region      = var.google_region
 }
 
-variable "google_credentials" {
-  description = "Base64 encoded Google Cloud service account key"
-}
-
-variable "google_project" {
-  description = "Google Cloud project ID"
-}
-
-variable "google_region" {
-  description = "Google Cloud region"
-  default     = "us-central1"  # or your preferred default region
+resource "google_compute_network" "vpc_network" {
+  name                    = "my-vpc-network"
+  auto_create_subnetworks = true
 }
 
 
