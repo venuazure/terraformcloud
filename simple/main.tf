836c7751234e3790f09d78106cd3051916f1c4fb
vijay <velagav4@outlook.com>
2024-07-28 11:29:45 +0530

    added code

--- simple/main.tf
+++ simple/main.tf
@@ -8,15 +8,20 @@ terraform {
   }
 }*/
 
+provider "google" {
+  credentials = var.GOOGLE_CREDENTIALS
+
+}
 resource "google_compute_network" "vpc_network" {
+
   name                    = var.vpc_network_name
   auto_create_subnetworks = true
 }
-/*
+
 variable "GOOGLE_CREDENTIALS" {
   description = "Base64-encoded contents of the service account JSON key file"
   type        = string
-}*/
+}
 
 variable "project" {
   description = "The ID of the GCP project to use"
