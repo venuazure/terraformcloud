ce704f5ba80da1bc575b916d1d59f5f1bdc483f7
vijay <velagav4@outlook.com>
2024-07-28 15:09:10 +0530

    added code-1

--- simple/main.tf
+++ simple/main.tf
@@ -1,9 +1,11 @@
-data "google_credentials" "default" {
-  json_key_base64 = var.google_credentials
-}
 
 provider "google" {
   credentials = data.google_credentials.default.json_key_base64
   project     = data.google_credentials.default.project_id
 }
+
+data "google_credentials" "default" {
+  json_key_base64 = var.google_credentials
+}
+
 
